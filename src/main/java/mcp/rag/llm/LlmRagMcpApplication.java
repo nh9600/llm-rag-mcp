f69cc83ebedce414162f7f5363dfad67827bc2bb
sky <nh9600@daum.net>
2026-09-21 23:36:07 +0900

    first commit

---
+++ src/main/java/mcp/rag/llm/LlmRagMcpApplication.java
@@ -0,0 +1,13 @@
+package mcp.rag.llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmRagMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmRagMcpApplication.class, args);
+	}
+
+}
